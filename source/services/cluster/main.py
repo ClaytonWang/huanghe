d18d1c3608055dac986787e4aafcdc2777c0e9cb
jiangshouchen <shouchen.jiang@digitalbrain.cn>
2022-11-24 15:04:20 +0800

    update cluster service main.py

--- source/services/cluster/main.py
+++ source/services/cluster/main.py
@@ -5,13 +5,7 @@ import os
 import uvicorn
 from fastapi import FastAPI
 
-# oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
-# configure_logging('logging.config.dictConfig', LOGGING)
 app = FastAPI()
-# # app.add_middleware(BaseHTTPMiddleware, dispatch=verify_token)
-
-# 路由配置
-# app.include_router(router_auth, prefix='/auth')
 
 
 @app.get('/status')
@@ -19,15 +13,17 @@ def status():
     return {"status": "ok"}
 
 
-service_port = os.getenv('USER_SERVICE_PORT', "").upper()
-if not service_port:
-    service_port = 80
-
-# USER_SERVICE_PORT
-if __name__ == '__main__':
+def start():
+    service_port = int(os.getenv('CLUSTER_SERVICE_PORT', ""))
+    if not service_port:
+        service_port = 80
     uvicorn.run(
-        'main:app',  port=service_port,
+        'main:app', port=service_port,
         reload=False,
         # debug=DEBUG,
         workers=2
     )
+
+# USER_SERVICE_PORT
+if __name__ == '__main__':
+    start()
