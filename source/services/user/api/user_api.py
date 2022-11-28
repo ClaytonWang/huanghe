652d16215afabe78a079a7958f97fd5404c61cbb
yjd <jindu.yin@digitalbrain.cn>
2022-11-28 09:28:24 +0800

    AIDP-15 增加删除

--- source/services/user/api/user_api.py
+++ source/services/user/api/user_api.py
@@ -5,11 +5,10 @@
     >Mail    : jindu.yin@digitalbrain.cn
     >Time    : 2022/10/13 07:10
 """
-from fastapi import APIRouter, Depends, HTTPException, status
-from fastapi import Query
+from fastapi import APIRouter, Depends, HTTPException, status, Path
+from fastapi.responses import JSONResponse
 from asyncpg.exceptions import UniqueViolationError
 from models.user import User
-from api.services import hash_password
 from api.serializers import UserList, UserCreate, UserEdit
 from basic.common.paginate import *
 from basic.common.query_filter_params import QueryParameters
@@ -24,7 +23,6 @@ router_user = APIRouter()
     response_model=UserList,
 )
 async def create_user(user: UserCreate):
-    # user.password = hash_password(user.password)
     try:
         return await User.objects.create(**user.dict())
     except UniqueViolationError as e:
@@ -48,8 +46,6 @@ async def list_user(
     return await paginate(User.objects.select_related('role'), params=query_params.params)
 
 
-from fastapi.responses import JSONResponse
-
 @router_user.put(
     '/{user_id}',
     description='更新用户信息',
@@ -57,7 +53,7 @@ from fastapi.responses import JSONResponse
 )
 async def update_user(
         user: UserEdit,
-        user_id: int = Query(..., ge=1, description='需要更新的用户ID'),
+        user_id: int = Path(..., ge=1, description='需要更新的用户ID'),
 ):
     update_data = user.dict(exclude_unset=True)
     print('update_data: ', update_data)
@@ -65,3 +61,18 @@ async def update_user(
     if not (_user and await _user.update(update_data)):
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='用户不存在')
     return JSONResponse(dict(id=user_id))
+
+
+@router_user.delete(
+    '/{user_id}',
+    description='删除用户',
+    status_code=status.HTTP_200_OK,
+    # response_model={"detail": "aaaa"},
+)
+async def delete_user(user_id: int = Path(..., ge=1, description='用户ID')):
+    user = await User.objects.get_or_none(id=user_id)
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='未查询到用户')
+
+    # TODO 判断用户管理资源
+    pass
