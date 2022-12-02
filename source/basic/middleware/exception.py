94e06df72c12d073b01580ea1118667faa4f55cb
yjd <jindu.yin@digitalbrain.cn>
2022-12-02 18:52:07 +0800

    AIDP-15 优化异常处理

--- source/basic/middleware/exception.py
+++ source/basic/middleware/exception.py
@@ -7,7 +7,7 @@
 """
 from fastapi import status
 from fastapi.responses import JSONResponse
-from asyncpg.exceptions import UniqueViolationError, ForeignKeyViolationError
+from asyncpg.exceptions import UniqueViolationError, ForeignKeyViolationError, DataError
 
 
 def validation_pydantic_exception_handler(request, exc):
@@ -34,9 +34,11 @@ def ormar_db_exception_handler(request, exc):
     :param exc:
     :return:
     """
-    if isinstance(exc, (UniqueViolationError, ForeignKeyViolationError)):
+    if isinstance(exc, (UniqueViolationError, ForeignKeyViolationError, DataError)):
         return JSONResponse(
-            exc.detail if hasattr(exc, 'detail') and len(str(exc.detail)) else str(exc),
+            exc.detail if hasattr(exc, 'detail')
+            and getattr(exc, 'detail') is not None
+            and len(str(exc.detail)) else str(exc),
             status_code=status.HTTP_400_BAD_REQUEST
         )
     return exc
