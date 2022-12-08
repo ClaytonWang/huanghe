517e8b2750ede9795581334dd872babe2cd4b0b3
jiangshouchen <shouchen.jiang@digitalbrain.cn>
2022-12-08 09:59:07 +0800

    add volume service
    delete cluster manger extra folder

---
+++ source/services/storage/models/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+    >File    : __init__.py
+    >Author  : YJD
+    >Mail    : jindu.yin@digitalbrain.cn
+    >Time    : 2022/10/11 09:29
+"""
+from models.initdb import *
+from models.volume import Volume
--- source/services/storage/volume/api.py
+++ source/services/storage/volume/api.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter, Depends, HTTPException, status, Path
 from fastapi.responses import JSONResponse
-from volume.serializers import
-from k8s.cluster_client import cc
+from models import Volume
+from volume.serializers import VolumeList
+# from k8s.cluster_client import cc
 
 
 router_volume = APIRouter()
@@ -10,21 +11,20 @@ router_volume = APIRouter()
 @router_volume.post(
     '',
     description='创建存储盘',
-    response_model=Namespace,
+    response_model=VolumeList,
 )
-def create_volume(volume: Volume):
-    cc.create_namespace(volume)
-    return ns
+async def create_volume(volume: Volume):
+    return await Volume.objects.create(**volume.dict())
 
 
 
 
 
-@router_volume.delete(
-    '',
-    description='删除',
-    response_model=Namespace,
-)
-def delete_volume(volume: Volume):
-    cc.delete_namespace(volume)
-    return ns
+# @router_volume.delete(
+#     '',
+#     description='删除',
+#     response_model=Namespace,
+# )
+# def delete_volume(volume: Volume):
+#     cc.delete_namespace(volume)
+#     return ns
