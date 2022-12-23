91ca75ed7f1847f8f047dd66ec28ef32a2962607
jiangshouchen <shouchen.jiang@digitalbrain.cn>
2022-12-23 17:09:24 +0800

    add cluster handler

--- source/services/cluster/notebook/api.py
+++ source/services/cluster/notebook/api.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Depends, HTTPException, status, Path
 from fastapi.responses import JSONResponse
 from notebook.serializers import NoteBookCreateReq, NoteBookDeleteReq, NoteBookListReq, NoteBook
 from k8s.cluster_client import cc
+from basic.middleware.rsp import success_common_response
 
 
 router_notebook = APIRouter()
@@ -10,11 +11,10 @@ router_notebook = APIRouter()
 @router_notebook.post(
     '',
     description='创建notebook',
-    response_model=NoteBookCreateReq,
 )
 def create_notebook(nbcr: NoteBookCreateReq):
     cc.create_notebook(NoteBook.parse_obj(nbcr.gen_notebook_dict()))
-    return nbcr
+    return success_common_response()
 
 
 @router_notebook.post(
@@ -30,8 +30,7 @@ def list_notebook(nblr: NoteBookListReq):
 @router_notebook.delete(
     '',
     description='删除notebook',
-    response_model=NoteBook,
 )
 def delete_notebook(nb: NoteBookDeleteReq):
     cc.delete_notebook(nb)
-    return nb
+    return success_common_response()
