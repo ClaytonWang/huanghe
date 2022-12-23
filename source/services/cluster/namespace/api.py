83d6df13f784e1550a35ba2d7ad9faaacc02b646
jiangshouchen <shouchen.jiang@digitalbrain.cn>
2022-12-23 17:09:24 +0800

    add cluster handler

--- source/services/cluster/namespace/api.py
+++ source/services/cluster/namespace/api.py
@@ -2,7 +2,8 @@ from fastapi import APIRouter, Depends, HTTPException, status, Path
 from fastapi.responses import JSONResponse
 from namespace.serializers import Namespace
 from k8s.cluster_client import cc
-
+from basic.middleware.rsp import success_common_response
+from kubernetes.client import ApiException
 
 router_namespace = APIRouter()
 
@@ -10,11 +11,10 @@ router_namespace = APIRouter()
 @router_namespace.post(
     '',
     description='创建命名空间',
-    response_model=Namespace,
 )
 def create_namespace(ns: Namespace):
     cc.create_namespace(ns)
-    return ns
+    return success_common_response()
 
 @router_namespace.delete(
     '',
@@ -23,4 +23,4 @@ def create_namespace(ns: Namespace):
 )
 def delete_namespace(ns: Namespace):
     cc.delete_namespace(ns)
-    return ns
+    return success_common_response()
--- source/services/cluster/pvc/api.py
+++ source/services/cluster/pvc/api.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Depends, HTTPException, status, Path
 from fastapi.responses import JSONResponse
 from pvc.serializers import PVC
 from k8s.cluster_client import cc
+from basic.middleware.rsp import success_common_response
 
 
 router_pvc = APIRouter()
@@ -10,11 +11,10 @@ router_pvc = APIRouter()
 @router_pvc.post(
     '',
     description='创建存储卷',
-    response_model=PVC,
 )
 def create_pvc(pvc: PVC):
     cc.create_namespaced_persistent_volume_claim(pvc)
-    return pvc
+    return success_common_response()
 #
 # @router_pvc.delete(
 #     '',
