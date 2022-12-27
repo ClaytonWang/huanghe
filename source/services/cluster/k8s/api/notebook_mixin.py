1da18dadf31876feae56ae9116130bcb30938024
jiangshouchen <shouchen.jiang@digitalbrain.cn>
2022-12-27 19:15:25 +0800

    modify interface

--- source/services/cluster/k8s/api/notebook_mixin.py
+++ source/services/cluster/k8s/api/notebook_mixin.py
@@ -8,46 +8,44 @@ from k8s.model.v1_notebook import V1Notebook
 from typing import Optional, Dict
 from notebook.serializers import NoteBook, NoteBookListReq, NoteBookDeleteReq
 
-
 NOTEBOOK_STATUS_RUNNING = "RUNNING"
 NOTEBOOK_STATUS_PENDING = "PENDING"
 NOTEBOOK_STATUS_ERROR = "ERROR"
 NOTEBOOK_STATUS_WAITING = "WAITING"
 NOTEBOOK_STATUS_ON = "ON"
 
+
 class NotebookMixin(CustomerObjectApi, CoreV1Api):
     def __init__(self, c: Core):
         super(NotebookMixin, self).__init__(c=c)
 
-
     def create_notebook(self, nb: NoteBook) -> Dict:
         return self.custom_object_api.create_namespaced_custom_object(group=KUBEFLOW_NOTEBOOK_GROUP,
-                                                                        version=KUBEFLOW_V1_VERSION,
-                                                                        namespace=nb.namespace,
-                                                                        plural=KUBEFLOW_NOTEBOOK_PLURAL,
-                                                                        body=V1Notebook.default(name=nb.name,
-                                                                                                namespace=nb.namespace,
-                                                                                                image=nb.image,
-                                                                                                labels=nb.labels,
-                                                                                                resource=nb.resource,
-                                                                                                envs=nb.envs,
-                                                                                                volumes=nb.volumes,
-                                                                                                tolerations=nb.tolerations,
-                                                                                                ),
-                                                                        )
-
+                                                                      version=KUBEFLOW_V1_VERSION,
+                                                                      namespace=nb.namespace,
+                                                                      plural=KUBEFLOW_NOTEBOOK_PLURAL,
+                                                                      body=V1Notebook.default(name=nb.name,
+                                                                                              namespace=nb.namespace,
+                                                                                              image=nb.image,
+                                                                                              labels=nb.labels,
+                                                                                              resource=nb.resource,
+                                                                                              envs=nb.envs,
+                                                                                              volumes=nb.volumes,
+                                                                                              tolerations=nb.tolerations,
+                                                                                              ),
+                                                                      )
 
     def delete_notebook(self, nbdr: NoteBookDeleteReq) -> V1Status:
         return self.custom_object_api.delete_namespaced_custom_object(name=nbdr.name, namespace=nbdr.namespace)
 
     def list_notebook(self, nblr: NoteBookListReq):
-        notebook_status_dic = {}
+        notebooks = []
 
         for notebook in self.custom_object_api.list_cluster_custom_object(group=KUBEFLOW_NOTEBOOK_GROUP,
-                                                                 version=KUBEFLOW_V1_VERSION,
-                                                                 plural=KUBEFLOW_NOTEBOOK_PLURAL,
-                                                                 label_selector=f"env={nblr.env}"
-                                                                    )['items']:
+                                                                          version=KUBEFLOW_V1_VERSION,
+                                                                          plural=KUBEFLOW_NOTEBOOK_PLURAL,
+                                                                          label_selector=f"env={nblr.env}"
+                                                                          )['items']:
             notebook_name = notebook["metadata"]['name']
             namespace = notebook["metadata"]['namespace']
             status = NOTEBOOK_STATUS_ON if self.is_ready(notebook) else NOTEBOOK_STATUS_PENDING
@@ -55,12 +53,12 @@ class NotebookMixin(CustomerObjectApi, CoreV1Api):
                 status, reason = NOTEBOOK_STATUS_ON, "success"
             else:
                 status, reason = self.process_notebook_status(notebook_name, namespace)
-            notebook_status_dic[notebook_name] = {"name": notebook_name,
-                                                  "namespace": namespace,
-                                                  "status": status,
-                                                  "reason": reason,
-                                                  "url": f"https://kubeflow.digitalbrain.cn:31443/notebook/{namespace}/{notebook_name}/lab"}
-        return notebook_status_dic
+            notebooks.append({"name": notebook_name,
+                              "namespace": namespace,
+                              "status": status,
+                              "reason": reason,
+                              "url": f"https://kubeflow.digitalbrain.cn:31443/notebook/{namespace}/{notebook_name}/lab"})
+        return notebooks
 
     def watch_notebook(self):
         return self.custom_object_api.list_namespaced_custom_object
@@ -76,7 +74,7 @@ class NotebookMixin(CustomerObjectApi, CoreV1Api):
             pod = self.core_v1_api.read_namespaced_pod("{}-0".format(name), namespace)
             # check init-container
             for init_container in pod.status.init_container_statuses:
-                    # check is finished
+                # check is finished
                 if init_container.state.terminated.exit_code == 0:
                     break
                 if init_container.state.running:
@@ -123,4 +121,4 @@ class NotebookMixin(CustomerObjectApi, CoreV1Api):
 
     def list_notebook_events(self, name, namespace):
         return self.core_v1_api.list_namespaced_event(namespace=namespace,
-                                                  field_selector="involvedObject.name={}-0".format(name))
+                                                      field_selector="involvedObject.name={}-0".format(name))
