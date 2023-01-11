18a66954fbd72842358faed6e590bcf5917f1e83
jiangshouchen <shouchen.jiang@digitalbrain.cn>
2023-01-11 15:44:29 +0800

    cluster add config

--- source/services/cluster/k8s/api/core.py
+++ source/services/cluster/k8s/api/core.py
@@ -1,12 +1,20 @@
-from kubernetes.config import load_kube_config
+import os
+from kubernetes.config import load_kube_config, load_incluster_config
 from kubernetes.client import CoreV1Api, CustomObjectsApi, StorageV1Api
 
 class Core():
     def __init__(self):
-        load_kube_config()
+        self._find_kubeconfig()
         self._core_v1_api = CoreV1Api()
         self._custom_object_api = CustomObjectsApi()
         self._storage_v1_api = StorageV1Api()
+
+
+    def _find_kubeconfig(self):
+        if os.getenv("KUBECONFIG"):
+            load_kube_config()
+        else:
+            load_incluster_config()
 
     @property
     def core_v1_api(self):
