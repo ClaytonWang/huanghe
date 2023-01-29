39b8120b66e736080a91635cdb9664b7b1761082
jiangshouchen <shouchen.jiang@digitalbrain.cn>
2023-01-29 15:13:23 +0800

    new service monitor

---
+++ source/services/monitor/models/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+    >File    : __init__.py
+    >Author  : YJD
+    >Mail    : jindu.yin@digitalbrain.cn
+    >Time    : 2022/10/11 09:29
+"""
+from models.initdb import *
