cc4937d61dc26705e093d968640611628cc856a2
jiangshouchen <shouchen.jiang@digitalbrain.cn>
2023-01-30 15:26:30 +0800

    fix bug

--- source/basic/common/validator_name.py
+++ source/basic/common/validator_name.py
@@ -9,10 +9,10 @@ class BaseModelValidatorName(BaseModel):
     def name_must_contain_space(cls, v):
         if not v[0].isalpha():
             raise ValueError('名称请由字母开头')
-        return v.title()
+        return v
 
     @validator('name')
     def name_include(cls, v):
         if not re.match('^[0-9a-zA-Z-]+$', v):
             raise ValueError('名称请由字母、数字、中划线组成')
-        return v.title()
+        return v
