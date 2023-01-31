da61398ad780f144ce3602fbab28129e7bd99ef3
guanlin.li <guanlin.li@digitalbrain.cn>
2023-01-31 15:37:37 +0800

    refactor: update mocks

---
+++ source/services/frontend/mock/user/pms/menu/GET.js
@@ -0,0 +1,35 @@
+/**
+ * @Author guanlin.li guanlin.li@digitalbrain.cn
+ * @Date 2023-01-31 15:26:15
+ * @LastEditors guanlin.li guanlin.li@digitalbrain.cn
+ * @LastEditTime 2023-01-31 15:28:16
+ */
+module.exports = {
+  success: true,
+  message: '',
+  status: 200,
+  result: [
+    {
+      id: 29,
+      code: '0003',
+      name: 'project',
+      value: '项目',
+      children: [
+        {
+          id: 30,
+          code: '00030001',
+          name: 'readonly',
+          value: '查看',
+          children: [],
+        },
+        {
+          id: 31,
+          code: '00030002',
+          name: 'edit',
+          value: '编辑',
+          children: [],
+        },
+      ],
+    },
+  ],
+};
