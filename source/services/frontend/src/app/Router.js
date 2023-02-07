a250e1a9642a9ea2db5646ad02391f7fb7e60a19
junshi <clayton.wang@digitalbrain.cn>
2023-02-07 16:28:13 +0800

    feat: notebook detail charts

--- source/services/frontend/src/app/Router.js
+++ source/services/frontend/src/app/Router.js
@@ -2,21 +2,24 @@
  * @Author: junshi clayton.wang@digitalbrain.cn
  * @Date: 2023-01-31 15:07:28
  * @LastEditors: junshi clayton.wang@digitalbrain.cn
- * @LastEditTime: 2023-02-03 17:06:19
+ * @LastEditTime: 2023-02-06 19:51:08
  * @FilePath: /huanghe/source/services/frontend/src/app/Router.js
  * @Description: Router Entry
  */
 import { AuthProvider } from '@/common/hooks/useAuth';
 import { MultiProvider } from '@/common/hooks/MultiProvider';
 import { RoutesProvider } from '@/common/hooks/RoutesProvider';
+import { ConfigProvider } from 'antd';
 import App from '@/app';
 import { HistoryRouter, history } from './history';
+import locale from 'antd/es/locale/zh_CN';
 import './index.less';
 
 const Router = () => (
   <HistoryRouter history={history}>
     <MultiProvider
       providers={[
+        <ConfigProvider locale={locale} key="config" />,
         <AuthProvider history={history} key="auth" />,
         <RoutesProvider key="routes" />,
       ]}
--- source/services/frontend/src/index.js
+++ source/services/frontend/src/index.js
@@ -4,9 +4,13 @@
  */
 import { createRoot } from 'react-dom/client';
 import Router from '@/app/Router';
+import moment from 'moment';
+import 'moment/locale/zh-cn';
 import './common/api';
 import './common/css/index.less';
 
+moment.locale('zh-cn');
+window.moment = moment;
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(<Router />);
