45319a2d83b4c4ca79125bf809f3a37ac8c96fa9
junshi <clayton.wang@digitalbrain.cn>
2023-02-08 10:28:52 +0800

    fix: notebook list && detai event bug fix

--- source/services/frontend/src/common/components/EventMonitor/index.js
+++ source/services/frontend/src/common/components/EventMonitor/index.js
@@ -2,7 +2,7 @@
  * @Author: junshi clayton.wang@digitalbrain.cn
  * @Date: 2023-02-01 18:13:47
  * @LastEditors: junshi clayton.wang@digitalbrain.cn
- * @LastEditTime: 2023-02-07 19:50:18
+ * @LastEditTime: 2023-02-08 10:27:00
  * @FilePath: /huanghe/source/services/frontend/src/common/components/EventMonitor/index.js
  * @Description: 事件监控
  */
@@ -22,7 +22,7 @@ const EventMonitor = ({
       dataIndex: 'status',
       width: '10%',
       render(_, record) {
-        return record?.status;
+        return record?.status?.desc || '-';
       },
     },
     {
