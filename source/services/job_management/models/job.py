3a2d4161f4885c4d3a4b131018fa63fb4a5bb48b
xy <xy>
2023-02-09 14:30:00 +0800

    modify job management

--- source/services/job_management/models/job.py
+++ source/services/job_management/models/job.py
@@ -15,14 +15,14 @@ from models import DB, META
 
 
 # 状态
-# JOB_STATUS_PENDING = "pending"  # 排队中
-# JOB_STATUS_RUNNING = "running"  # 已启动(运行中)
-# JOB_STATUS_STOP = "stop"  # 停止中
-# JOB_STATUS_START_FAIL = "start_fail"  # 启动失败
-# JOB_STATUS_RUN_FAIL = "run_fail"  # 运行失败
-# JOB_STATUS_STOP_FAIL = "stop_fail"  # 停止失败
-# JOB_STATUS_ON = "on"  # 已完成
-# JOB_STATUS_STOPPED = "stopped"  # 已停止
+# JOB_STATUS_PENDING = "pending"  # 排队中     3
+# JOB_STATUS_RUNNING = "running"  # 已启动(运行中)    4
+# JOB_STATUS_STOP = "stop"  # 停止中       2
+# JOB_STATUS_START_FAIL = "start_fail"  # 启动失败  7
+# JOB_STATUS_RUN_FAIL = "run_fail"  # 运行失败  8
+# JOB_STATUS_STOP_FAIL = "stop_fail"  # 停止失败    9
+# JOB_STATUS_ON = "completed"  # 已完成       10
+# JOB_STATUS_STOPPED = "stopped"  # 已停止 5
 
 
 class Status(ormar.Model):
