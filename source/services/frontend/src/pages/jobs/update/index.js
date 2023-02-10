40857be08f1f66faf6746657c704e7f8e1def922
junshi <clayton.wang@digitalbrain.cn>
2023-02-10 11:49:26 +0800

    feat: create job

--- source/services/frontend/src/pages/jobs/update/index.js
+++ source/services/frontend/src/pages/jobs/update/index.js
@@ -1,14 +1,23 @@
-/*
- * @Author: junshi clayton.wang@digitalbrain.cn
- * @Date: 2023-01-31 16:10:19
- * @LastEditors: junshi clayton.wang@digitalbrain.cn
- * @LastEditTime: 2023-01-31 17:29:19
- * @FilePath: /frontend/src/pages/jobs/update/index.js
- * @Description: Job新建 / 编辑页;
+/**
+ * @Author guanlin.li guanlin.li@digitalbrain.cn
+ * @Date 2022-12-22 10:48:26
+ * @LastEditors guanlin.li guanlin.li@digitalbrain.cn
+ * @LastEditTime 2022-12-23 09:40:59
+ * @Description Job 新建/编辑页
  */
 import { useEffect, useState, useMemo, useRef } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
-import { Button, Divider, Form, Input, message, Select, Tooltip } from 'antd';
+import {
+  Button,
+  Divider,
+  Form,
+  Input,
+  message,
+  Select,
+  Tooltip,
+  Checkbox,
+  Space,
+} from 'antd';
 import { uniqueId, get, map, drop } from 'lodash';
 import {
   DeleteFilled,
@@ -17,11 +26,18 @@ import {
 } from '@ant-design/icons';
 import api from '@/common/api';
 import { CREATE, UPDATE, ADMIN } from '@/common/constants';
+import { useContextProps } from '@/common/hooks/RoutesProvider';
 import { useAuth } from '@/common/hooks/useAuth';
 import { genUniqueIdByPrefix, ID } from '@/common/utils/helper';
 import './index.less';
 
 const { Option } = Select;
+const { TextArea } = Input;
+
+const taskModels = [
+  { id: 0, name: '调试' },
+  { id: 1, name: '非调试' },
+];
 
 const JobsUpdate = () => {
   const [projectsDatasource, setProjectsDatasource] = useState([]);
@@ -29,8 +45,10 @@ const JobsUpdate = () => {
   const [sourceDatasource, setSourceDatasource] = useState([]);
   const [storagesDatasource, setStoragesDatasource] = useState([]);
   const [selectedStorages, setSelectedStorages] = useState([]);
+  const [taskModel, setTaskModel] = useState(0);
+  const setContextProps = useContextProps();
   const [type, setType] = useState(CREATE);
-  const notebookUniqueID = useRef();
+  const uniqueID = useRef();
   const { user } = useAuth();
   const navigate = useNavigate();
   const location = useLocation();
@@ -41,14 +59,14 @@ const JobsUpdate = () => {
     if (transform) {
       result = transform(values);
     } else {
-      result = map(get(values, 'hooks', []), 'storage');
+      result = map(get(values, 'hooks', []), 'storage').map(({ id }) => id);
     }
     setSelectedStorages([...result]);
   };
 
-  const requestNotebook = async (params) => {
+  const requestJob = async (params) => {
     try {
-      const { result } = await api.notebooksDetail({ ...params });
+      const { result } = await api.jobDetail({ ...params });
       form.setFieldsValue(result);
       updateSelectedStorage(result);
     } catch (error) {
@@ -69,6 +87,16 @@ const JobsUpdate = () => {
       console.log(error);
     }
   };
+
+  const projectDefaultValue = useMemo(() => {
+    if (projectsDatasource && projectsDatasource.length > 0) {
+      return {
+        name: projectsDatasource[0]?.name,
+        id: projectsDatasource[0]?.id,
+      };
+    }
+  }, [projectsDatasource]);
+
   const requestImages = async () => {
     try {
       const { result } = await api.imagesList();
@@ -77,6 +105,17 @@ const JobsUpdate = () => {
       console.log(error);
     }
   };
+
+  const imageDefaultValue = useMemo(() => {
+    if (imagesDatasource && imagesDatasource.length > 0) {
+      return {
+        custom: false,
+        id: imagesDatasource[0]?.id,
+        name: imagesDatasource[0]?.name,
+      };
+    }
+  }, [imagesDatasource]);
+
   const requestSource = async () => {
     try {
       const { result } = await api.sourceList();
@@ -97,18 +136,18 @@ const JobsUpdate = () => {
       console.log(error);
     }
   };
-  const saveNotebook = async (values) => {
+  const saveJob = async (values) => {
     try {
-      await api.notebooksListCreate(values);
+      await api.jobListCreate(values);
       message.success('创建成功！');
       backToList();
     } catch (error) {
       console.log(error);
     }
   };
-  const updateNotebook = async (values) => {
+  const updateJob = async (values) => {
     try {
-      await api.notebooksListUpdate(values);
+      await api.jobListUpdate(values);
       message.success('保存成功!');
       backToList();
     } catch (error) {
@@ -137,17 +176,20 @@ const JobsUpdate = () => {
     } else if (changedHooks) {
       // 删除/新增挂载，通过hooks下的value数组，更新selectedStorage集合
       updateSelectedStorage(get(changedHooks, 'value', []), (values) =>
-        values.map(({ storage }) => storage || false).filter((value) => value)
+        values
+          .map(({ storage }) => storage?.id || false)
+          .filter((value) => value)
       );
     }
   };
   const handleSubmit = () => {
     const values = form.getFieldsValue();
     const { id = null } = get(location, 'state.params', {});
+    console.log(values);
     if (type === CREATE) {
-      saveNotebook(values);
+      saveJob(values);
     } else {
-      updateNotebook({ id, ...values });
+      updateJob({ id, ...values });
     }
   };
   const handleSubmitFailed = ({ errorFields }) => {
@@ -168,30 +210,35 @@ const JobsUpdate = () => {
     requestImages();
     requestSource();
     requestStorages();
-    notebookUniqueID.current = new ID();
+    uniqueID.current = new ID();
+    setContextProps({
+      onCancel: handleCancelClicked,
+      onSubmit: () => {
+        form.submit();
+      },
+    });
   }, []);
 
   useEffect(() => {
     const { id = null } = get(location, 'state.params', {});
     const type = get(location, 'state.type');
     if (type === UPDATE) {
-      requestNotebook({ id });
+      requestJob({ id });
     } else {
       form.setFieldsValue({
-        hooks: [
-          {
-            path: '/home/jovyan',
-          },
-        ],
+        project: projectDefaultValue,
+        image: imageDefaultValue,
+        task_model: taskModel,
       });
     }
     setType(type);
-  }, [location]);
+  }, [location, form, projectDefaultValue, imageDefaultValue]);
 
   const HooksItem = ({ name, remove, selectedStorages, disabledItems }) => {
-    const currStorage = form.getFieldValue(['hooks', name, 'storage']);
+    const currStorage = form.getFieldValue(['hooks', name, 'storage', 'id']);
     const filteredStorageOptions = useMemo(() => {
       const result = storagesDatasource.filter(
+        // 非选中了的存储集合的元素及当前选择的存储元素的集合
         ({ id }) => !(selectedStorages.includes(id) && id !== currStorage)
       );
       return result;
@@ -201,7 +248,7 @@ const JobsUpdate = () => {
         <div className="notebooks-hooks-item">
           <span className="content">
             <Form.Item
-              name={[name, 'storage']}
+              name={[name, 'storage', 'id']}
               label="存储盘"
               rules={[{ required: true, message: '请选择存储盘' }]}
             >
@@ -254,6 +301,103 @@ const JobsUpdate = () => {
       </>
     );
   };
+
+  const checkImage = (_, value) => {
+    if (value?.name) {
+      return Promise.resolve();
+    }
+    return Promise.reject(new Error('请输入镜像！'));
+  };
+
+  const CustomImage = ({ value, onChange }) => {
+    const [custom, setCustom] = useState(value?.custom);
+    const onCheckChange = (e) => {
+      setCustom(e.target.checked);
+      onChange?.({
+        custom: e.target.checked,
+        name: null,
+      });
+    };
+    const onInputChange = (e) => {
+      onChange?.({ custom: true, name: e.target.value });
+    };
+    const onSelectChange = (value) => {
+      onChange?.({ custom: false, name: value });
+    };
+    return (
+      <>
+        {custom ? (
+          <Input
+            addonBefore="harbor.digitalbrain.cn/"
+            placeholder="输入镜像地址"
+            onChange={onInputChange}
+            value={value?.name}
+          />
+        ) : (
+          <Select
+            placeholder="请选择镜像"
+            showSearch
+            filterOption={(input, option) =>
+              (option?.children ?? '').includes(input)
+            }
+            defaultValue={value?.name}
+            onChange={onSelectChange}
+          >
+            {imagesDatasource.map(({ id, name }) => (
+              <Option key={id} value={name}>
+                <Tooltip title={name}>{name}</Tooltip>
+              </Option>
+            ))}
+          </Select>
+        )}
+
+        <Checkbox checked={custom} onChange={onCheckChange}>
+          自定义镜像
+          <Tooltip title="请填写公共镜像仓库，如：tensorflow:1.13">
+            <InfoCircleOutlined style={{ marginLeft: 5, cursor: 'help' }} />
+          </Tooltip>
+        </Checkbox>
+      </>
+    );
+  };
+  const ProjectSelect = ({ value, onChange }) => {
+    const onSelectChange = (value) => {
+      onChange?.({ id: value });
+    };
+    return (
+      <Select
+        placeholder="请选择项目"
+        defaultValue={value?.id}
+        onChange={onSelectChange}
+      >
+        {projectsDatasource.map(({ id, name = '-' }) => (
+          <Option key={id} value={id}>
+            {name}
+          </Option>
+        ))}
+      </Select>
+    );
+  };
+
+  const JobModel = ({ value, onChange }) => {
+    const onSelectChange = (value) => {
+      onChange?.(value);
+      setTaskModel(value);
+    };
+    return (
+      <Select
+        placeholder="请选择任务模式"
+        defaultValue={value || 0}
+        onChange={onSelectChange}
+      >
+        {taskModels.map(({ id, name = '-' }) => (
+          <Option key={id} value={id}>
+            {name}
+          </Option>
+        ))}
+      </Select>
+    );
+  };
   return (
     <div className="notebooks-update">
       <Form
@@ -269,7 +413,7 @@ const JobsUpdate = () => {
           name="name"
           label="名称"
           rules={[
-            { required: true, message: '请输入Notebook名称' },
+            { required: true, message: '请输入Job名称' },
             {
               pattern: /^[a-zA-z][0-9a-zA-Z-]*$/,
               message: '字母开头，可以是字母、数字、中划线组合',
@@ -281,82 +425,19 @@ const JobsUpdate = () => {
             icon: <InfoCircleOutlined />,
           }}
         >
-          <Input placeholder="请输入Notebook名称" />
-        </Form.Item>
-        <Form.Item
-          name="image"
-          label="镜像"
-          rules={[{ required: true, message: '请选择镜像' }]}
-        >
-          <Select
-            placeholder="请选择镜像"
-            showSearch
-            filterOption={(input, option) =>
-              (option?.children ?? '').includes(input)
-            }
-          >
-            {imagesDatasource.map(({ id, name }) => (
-              <Option key={id} value={id}>
-                <Tooltip title={name}>{name}</Tooltip>
-              </Option>
-            ))}
-          </Select>
-        </Form.Item>
-        <Form.Item
-          name="source"
-          label="资源规格"
-          rules={[{ required: true, message: '请选择资源规格' }]}
-        >
-          <Select
-            placeholder="请选择资源规格"
-            showSearch
-            filterOption={(input, option) =>
-              (option?.children ?? '').includes(input)
-            }
-          >
-            {sourceDatasource.map(({ id, name }) => (
-              <Option key={id} value={id}>
-                <Tooltip title={name}>{name}</Tooltip>
-              </Option>
-            ))}
-          </Select>
+          <Input placeholder="请输入Job名称" />
         </Form.Item>
         <Form.Item
           name="project"
-          label="所属项目"
+          label="项目"
           rules={[{ required: true, message: '请选择项目' }]}
         >
-          <Select placeholder="请选择项目">
-            {projectsDatasource.map(({ id, name = '-' }) => (
-              <Option key={id} value={id}>
-                {name}
-              </Option>
-            ))}
-          </Select>
+          <ProjectSelect />
         </Form.Item>
-        <Form.List
-          name="hooks"
-          rules={[
-            {
-              validator: async (_, hooks) => {
-                if (!hooks || hooks.length < 1) {
-                  return Promise.reject(new Error('至少一个挂载'));
-                }
-              },
-            },
-          ]}
-        >
+        <Form.List name="hooks">
           {(fields = [], { add, remove }, { errors }) => (
             <>
               <Form.Item label="存储挂载" wrapperCol={{ push: 2, xs: 22 }}>
-                {
-                  <HooksItem
-                    key={uniqueId('hook-')}
-                    {...fields[0]}
-                    selectedStorages={selectedStorages}
-                    disabledItems={['path']}
-                  />
-                }
                 {drop([...fields], 1).map((field) => (
                   <HooksItem
                     key={uniqueId('hook-')}
@@ -371,7 +452,7 @@ const JobsUpdate = () => {
                     add({
                       path: `/home/jovyan/${genUniqueIdByPrefix(
                         'vol-',
-                        notebookUniqueID.current
+                        uniqueID.current
                       )}`,
                     })
                   }
@@ -384,14 +465,82 @@ const JobsUpdate = () => {
             </>
           )}
         </Form.List>
-        <Form.Item>
-          <Button onClick={handleCancelClicked}>取消</Button>
-          <Button type="primary" htmlType="submit">
-            保存
-          </Button>
+        <Form.Item
+          name="task_model"
+          label="模式"
+          rules={[{ required: true, message: '请选择模式' }]}
+        >
+          <JobModel />
+        </Form.Item>
+        {taskModel === 1 && (
+          <Form.Item
+            name="start_command"
+            label="启动命令"
+            tooltip={{
+              title: '...',
+              icon: <InfoCircleOutlined />,
+            }}
+            rules={[{ required: true, message: '请输入启动命令' }]}
+          >
+            <TextArea rows={4} placeholder="输入..." />
+          </Form.Item>
+        )}
+
+        <Form.Item
+          name="image"
+          label="镜像"
+          rules={[
+            { required: true, message: '请选择镜像' },
+            {
+              validator: checkImage,
+            },
+          ]}
+        >
+          <CustomImage />
+        </Form.Item>
+        <Form.Item
+          name="work_dir"
+          label="工作路径"
+          tooltip={{
+            title: '输入容器的工作目录',
+            icon: <InfoCircleOutlined />,
+          }}
+          rules={[{ required: false }]}
+        >
+          <Input placeholder="输入容器的工作目录" />
+        </Form.Item>
+        <Form.Item
+          name="source"
+          label="资源规格"
+          rules={[{ required: true, message: '请选择资源规格' }]}
+        >
+          <Select
+            placeholder="请选择资源规格"
+            showSearch
+            filterOption={(input, option) =>
+              (option?.children ?? '').includes(input)
+            }
+          >
+            {sourceDatasource.map(({ id, name }) => (
+              <Option key={id} value={name}>
+                <Tooltip title={name}>{name}</Tooltip>
+              </Option>
+            ))}
+          </Select>
         </Form.Item>
       </Form>
     </div>
   );
 };
+JobsUpdate.context = ({ onCancel, onSubmit }) => (
+  <Space>
+    <Button onClick={onCancel}>取消</Button>
+    <Button type="primary" onClick={onSubmit}>
+      保存
+    </Button>
+  </Space>
+);
+
+JobsUpdate.path = ['/jobs/list/update', '/jobs/list/create'];
+
 export default JobsUpdate;
