f911b110dd79071834b2fa448f287198cd5ef627
lingyi <lingyi.zhou@digitalbrain.cn>
2023-02-16 16:15:35 +0800

    fix server list

--- source/services/monitor/utils/k8s_request.py
+++ source/services/monitor/utils/k8s_request.py
@@ -5,9 +5,9 @@ async def list_server_k8s():
     async with aiohttp.ClientSession() as session:
         # url = "http://127.0.0.1:8001/server"
         url = f"{ENV_COMMON_URL}{CLUSTER_SERVER_PREFIX_URL}"
-        headers = {
-            'Content-Type': 'application/json'
-        }
-        async with session.get(url, headers=headers) as response:
+        # headers = {
+        #     'Content-Type': 'application/json'
+        # }
+        async with session.get(url) as response:
             response = await response.json()
             return response['result']
