ab563ec119fe2f19eb566fc09bebf379172ee684
junshi <clayton.wang@digitalbrain.cn>
2023-02-16 12:06:57 +0800

    fix: edit job

--- source/services/frontend/src/pages/overview/summary/index.js
+++ source/services/frontend/src/pages/overview/summary/index.js
@@ -2,30 +2,33 @@
  * @Author: junshi clayton.wang@digitalbrain.cn
  * @Date: 2023-01-31 15:07:28
  * @LastEditors: junshi clayton.wang@digitalbrain.cn
- * @LastEditTime: 2023-02-15 18:18:39
+ * @LastEditTime: 2023-02-16 12:02:53
  * @FilePath: /huanghe/source/services/frontend/src/pages/overview/summary/index.js
  * @Description: Overview Summary page
  */
-import { useEffect, useState, useMemo, useRef } from 'react';
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import { Select, Space, Row, Col, Card, Statistic } from 'antd';
-import { CREATE, UPDATE, ADMIN } from '@/common/constants';
+import { DownOutlined, UpOutlined, RightOutlined } from '@ant-design/icons';
+import { ADMIN } from '@/common/constants';
 import { useAuth } from '@/common/hooks/useAuth';
 import api from '@/common/api';
 import CountUp from 'react-countup';
 import './index.less';
 
-const { Option } = Select;
-
 const formatter = (value) => <CountUp end={value} separator="," />;
 
-const StatisticCard = ({ title, total = 0, run = 0, onClick }) => (
+const StatisticCard = ({ title, total = 0, run = 0 }) => (
   <Card.Grid style={{ width: '100%', padding: 15 }}>
     <Row justify="space-between">
       <Col span={12} style={{ fontWeight: 'bold' }}>
         {title}
       </Col>
-      <Col span={6} offset={6} style={{ textAlign: 'right' }}>
-        <a onClick={onClick}>查看详情</a>
+      <Col span={12} style={{ textAlign: 'right' }}>
+        <Link to={`/overview/serverlist`}>
+          查看详情
+          <RightOutlined />
+        </Link>
       </Col>
     </Row>
     <Row style={{ marginTop: 10 }}>
@@ -46,6 +49,7 @@ const SourceStatisticCard = ({
   occupied = 0,
   used = 0,
   occupied_rate = 0,
+  suffix = 'C',
 }) => (
   <Card.Grid style={{ width: '100%', padding: 15 }}>
     <Row>
@@ -55,11 +59,11 @@ const SourceStatisticCard = ({
     </Row>
     <Row style={{ marginTop: 10 }}>
       <Col span={8}>
-        <Statistic value={occupied} formatter={formatter} suffix="C" />
+        <Statistic value={occupied} formatter={formatter} suffix={suffix} />
         已占用
       </Col>
       <Col span={8} style={{ textAlign: 'center' }}>
-        <Statistic value={used} formatter={formatter} suffix="C" />
+        <Statistic value={used} formatter={formatter} suffix={suffix} />
         已使用
       </Col>
       <Col span={8} style={{ textAlign: 'right' }}>
@@ -70,9 +74,24 @@ const SourceStatisticCard = ({
   </Card.Grid>
 );
 
+const OverviewChartMonitor = () => {
+  const grafana = `https://grafana.digitalbrain.cn:32443/d/l_ZkLT5Vz
+/ji-qun-jian-kong?orgId=1&from=now-3h&to=now&theme=light&kiosk=tv&refresh=10s`;
+
+  return (
+    <div className="overview-list">
+      <iframe name="iframe" className="content" src={grafana} />
+    </div>
+  );
+};
+
 const OverviewList = () => {
   const [projectsDatasource, setProjectsDatasource] = useState([]);
   const [selectedProj, setSelectedProj] = useState([]);
+  const [tasksData, setTasksData] = useState();
+  const [sourceData, setSourceData] = useState();
+
+  const [open, setOpen] = useState(false);
   const { user } = useAuth();
 
   const options = [];
@@ -102,6 +121,27 @@ const OverviewList = () => {
     }
   };
 
+  const requestTasks = async (project) => {
+    try {
+      const params = { project };
+      const { result } = await api.serverTask(params);
+      setTasksData(result);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const requestSource = async (project) => {
+    try {
+      console.log(project);
+      const params = { project };
+      const { result } = await api.serverSource(params);
+      setSourceData(result);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   /* eslint-disable react-hooks/exhaustive-deps */
   useEffect(() => {
     requestProjects();
@@ -111,6 +151,8 @@ const OverviewList = () => {
     if (projectsDatasource && projectsDatasource.length > 0) {
       const _data = projectsDatasource.map(({ id }) => id);
       setSelectedProj(_data);
+      requestTasks(_data);
+      requestSource(_data);
     }
   }, [projectsDatasource]);
 
@@ -159,24 +201,34 @@ const OverviewList = () => {
             <SourceStatisticCard title="GPU" />
           </Col>
           <Col span={6}>
-            <SourceStatisticCard title="内存" />
+            <SourceStatisticCard title="内存" suffix="T" />
           </Col>
           <Col span={6}>
-            <SourceStatisticCard title="存储" />
+            <SourceStatisticCard title="存储" suffix="T" />
           </Col>
         </Row>
+        <Row style={{ marginTop: 15 }}>
+          <Col span={24}>
+            <a
+              onClick={() => {
+                setOpen(!open);
+              }}
+            >
+              {open ? '收起' : '展开'}
+              {open ? <UpOutlined /> : <DownOutlined />}
+            </a>
+          </Col>
+        </Row>
+        {open ? (
+          <Row>
+            <Col span={24}>
+              <OverviewChartMonitor />
+            </Col>
+          </Row>
+        ) : null}
       </Card>
     </div>
   );
 };
-
-// const grafana = `https://grafana.digitalbrain.cn:32443/d/l_ZkLT5Vz
-// /ji-qun-jian-kong?orgId=1&from=now-3h&to=now&theme=light&kiosk=tv&refresh=10s `;
-
-// return (
-//   <div className="overview-list">
-//     <iframe name="iframe" className="content" src={grafana} />
-//   </div>
-// );
 
 export default OverviewList;
