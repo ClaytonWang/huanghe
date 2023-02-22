d66ee68ba2fc285a1e2bb12aee793456a7d320e7
menglingbo <lingbo.meng@digitalbrain.cn>
2023-02-22 19:35:35 +0800

    fix server list

--- source/services/monitor/scheduler_task.py
+++ source/services/monitor/scheduler_task.py
@@ -6,7 +6,7 @@ from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from apscheduler.events import EVENT_JOB_EXECUTED, EVENT_JOB_ERROR
 from models.initdb import startup_event
 from models.server import Server
-from models.user import User
+
 from utils.k8s_request import list_server_ip_k8s
 from utils.service_requests import get_notebook_job_list_by_server
 
