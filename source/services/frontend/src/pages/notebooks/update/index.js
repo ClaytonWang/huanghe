2a1c7245bd8c46c1b3b6b746419f7c6f7ae432fd
guanlin.li <guanlin.li@digitalbrain.cn>
2023-02-24 19:17:19 +0800

    feat: docker url update

--- source/services/frontend/src/pages/notebooks/update/index.js
+++ source/services/frontend/src/pages/notebooks/update/index.js
@@ -345,7 +345,7 @@ const NotebooksUpdate = () => {
       <>
         {custom ? (
           <Input
-            addonBefore="harbor.digitalbrain.cn/"
+            addonBefore="swr.cn-north-4.myhuaweicloud.com/"
             placeholder="输入镜像地址"
             onChange={onInputChange}
             value={value?.name}
