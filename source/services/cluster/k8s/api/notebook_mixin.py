eb0813543e8279782a4d23da5f5c045f586853fb
menglingbo <lingbo.meng@digitalbrain.cn>
2023-02-27 15:20:13 +0800

    fix server list

--- source/services/cluster/k8s/api/notebook_mixin.py
+++ source/services/cluster/k8s/api/notebook_mixin.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-from typing import List
 
 from k8s.api.core import Core
 from k8s.model.v1_status import V1Status
@@ -8,7 +7,7 @@ from k8s.api.custom_object_api import CustomerObjectApi
 from k8s.api.core_v1_api import CoreV1Api
 from k8s.const.crd_kubeflow_const import KUBEFLOW_NOTEBOOK_GROUP, KUBEFLOW_V1_VERSION, KUBEFLOW_NOTEBOOK_PLURAL
 from k8s.model.v1_notebook import V1Notebook
-from typing import Dict
+from typing import Optional, Dict
 from notebook.serializers import NoteBook, NoteBookListReq, NoteBookDeleteReq
 from basic.config.cluster import KUBEFLOW_NOTEBOOK_URL
 
