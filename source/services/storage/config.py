39a83ff136711a843ee72a53c046ab954e1b4f8d
xinkai.tao <xinkai.tao@digitalbrain.cn>
2023-03-13 11:32:39 +0800

    update config file for other service

--- source/services/storage/config.py
+++ source/services/storage/config.py
@@ -11,18 +11,13 @@ import yaml
 import importlib
 from pathlib import Path
 
-DEBUG = True
-SERVICE_PORT = 8000
-DO_NOT_AUTH_URI = ['/auth/login', '/docs', '/openapi', '/openapi.json']
-NO_AUTH_WORDS = ['events']
 
 APP_NAME = Path(__file__).parent.name
 BASIC_PATH = Path.joinpath(Path(__file__).parent.parent.parent, 'basic')
 SOURCE_PATH = Path.joinpath(Path(BASIC_PATH).parent)
 sys.path.insert(0, SOURCE_PATH.__str__())
 K8S_YAML_CONFIG_PATH = '/etc/juece/config.yaml'
-COMMON_CONFIG_PATH = f'basic.config.{APP_NAME}'
-SERVICE_CONFIG_PATH = 'basic.config.service_requests'
+COMMON_CONFIG_PATH = 'basic.common.base_config'
 
 
 try:
@@ -36,23 +31,10 @@ except ModuleNotFoundError:
     pass
 
 
-# 从服务间请求加载
-try:
-    # print(SERVICE_CONFIG_PATH)
-    service_module = importlib.import_module(SERVICE_CONFIG_PATH)
-    for key in service_module.__dict__:
-        if key.startswith('__'):
-            continue
-        val = getattr(service_module, key)
-        locals().__setitem__(key, val)
-except ModuleNotFoundError:
-    pass
-
-DB_USER = 'root'
-DB_PASSWORD = 'linshimima2!'
-DB_NAME = 'huanghe_dev'
-DB_HOST = '123.60.43.172'
-DB_PORT = '5432'
+# DEBUG = True
+# SERVICE_PORT = 8000
+# DO_NOT_AUTH_URI = ['/auth/login', '/docs', '/openapi', '/openapi.json']
+# NO_AUTH_WORDS = ['events']
 
 
 if os.path.exists(K8S_YAML_CONFIG_PATH):
