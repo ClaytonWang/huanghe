cee6f93b5bdb422bf0a38ed5e9246d2f60fb0fa6
menglingbo <lingbo.meng@digitalbrain.cn>
2023-03-13 19:37:47 +0800

    fix bug use object instead of dict

--- source/services/cluster/k8s/api/server_mixin.py
+++ source/services/cluster/k8s/api/server_mixin.py
@@ -4,12 +4,12 @@ from services.cluster.k8s.api.core_v1_api import CoreV1Api
 
 def check_status(status_data):
     for condition in status_data:
-        status = condition['type']
+        status = condition.type
         if "Pressure" in status:
-            if condition['status'] == "True":
+            if condition.status == "True":
                 return "False"
         if status == 'Ready':
-            if condition['status'] == "True":
+            if condition.status == "True":
                 return "Success"
             else:
                 return "False"
@@ -25,15 +25,15 @@ class ServerMixin(CoreV1Api):
 
     def get_server_list(self):
         nodes = []
-        response = self.core_v1_api.list_node_with_http_info()[0].to_dict()
-        for node in response['items']:
-            server = node['metadata']['name']
-            node_source_data = node['status']['capacity']
-            server_ip = node['status']['addresses'][0]['address']
+        response = self.core_v1_api.list_node_with_http_info()[0].items
+        for node in response:
+            server = node.metadata.name
+            node_source_data = node.status.capacity
+            server_ip = node.status.addresses[0].address
             cpu = node_source_data['cpu']
             memory = translate_memory(node_source_data['memory'])
-            status = check_status(node['status']['conditions'])
-            if node_source_data.get("nvidia.com/gpu") is None:
+            status = check_status(node.status.conditions)
+            if not node_source_data.get("nvidia.com/gpu"):
                 nodes.append({
                     'status': status,
                     'server_ip': server_ip,
@@ -43,7 +43,7 @@ class ServerMixin(CoreV1Api):
                 })
             else:
                 gpu = node_source_data["nvidia.com/gpu"]
-                type = node["metadata"]["labels"]["nvidia.com/gpu.product"].split("-")[1]
+                type = node.metadata.labels["nvidia.com/gpu.product"].split("-")[1]
                 nodes.append({
                     'status': status,
                     'server_ip': server_ip,
