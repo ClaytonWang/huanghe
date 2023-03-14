8b3c82a4bf3c852c0824a8a53f88cb47aafed42a
xinkai.tao <xinkai.tao@digitalbrain.cn>
2023-03-14 10:55:18 +0800

    remove useless code

--- source/services/monitor/utils/service_requests.py
+++ source/services/monitor/utils/service_requests.py
@@ -8,86 +8,9 @@
 
 import aiohttp
 from typing import Optional, List, Set
-from pydantic import BaseModel, Field
+from pydantic import BaseModel
 from basic.common.base_config import *
 import requests
-
-
-class RoleInfo(BaseModel):
-    id: int
-    name: str
-
-
-class UserInfo(BaseModel):
-    id: int = Field(..., alias='user_id')
-    username: str = Field(..., alias='user_name')
-    en_name: str = Field(..., )
-    role: RoleInfo
-    permissions: Optional[List[str]]
-    project_ids: Set
-
-    class Config:
-        orm_mode = True
-        allow_population_by_field_name = True
-
-    def get_dict(self):
-        return {
-            "id": self.id,
-            "username": self.username,
-            "en_name": self.en_name,
-            "role_id": self.role.id,
-            "role_name": self.role.name,
-            "permissions": self.permissions,
-            "project_ids": self.project_ids,
-        }
-
-
-class NotebookInfo(BaseModel):
-    id: int
-    name: str
-    project_id: int
-    status: str
-    cpu: int
-    memory: int
-    gpu: int
-    storage_value: int
-    storage_size: int
-
-    class Config:
-        orm_mode = True
-        allow_population_by_field_name = True
-
-    def get_dict(self):
-        return {
-            'id': self.id,
-            'name': self.name,
-            'project_id': self.project_id,
-            'status': self.status,
-            'cpu': self.cpu,
-            'memory': self.memory,
-            'gpu': self.gpu,
-            'storage_value': self.storage_value,
-            'storage_size': self.storage_size,
-        }
-
-
-class JobInfo(BaseModel):
-    id: int
-    name: str
-    project_id: int
-    status: str
-
-    class Config:
-        orm_mode = True
-        allow_population_by_field_name = True
-
-    def get_dict(self):
-        return {
-            'id': self.id,
-            'name': self.name,
-            'project_id': self.project_id,
-            'status': self.status,
-        }
 
 
 class PodInfoByServer(BaseModel):
@@ -109,49 +32,6 @@ class PodInfoByServer(BaseModel):
             'memory': self.memory,
             'created_by': self.created_by
         }
-
-
-async def get_user_list(token):
-    async with aiohttp.ClientSession() as session:
-        # url = USER_SERVICE_PATH + f"/user?pagesize=100&pageno={page_no}"
-        # url = f"{ENV_COMMON_URL}{USER_PREFIX_URL}/items"
-        url = f"http://{USER_SERVICE_URL}{USER_PREFIX_URL}/items"
-        headers = {
-            'Authorization': token,
-            'Content-Type': 'application/json'
-        }
-        async with session.get(url, headers=headers) as response:
-            # print("status:{}".format(response.status))
-            text = await response.json()
-            user_data = text['result']
-            res = []
-            for user in user_data:
-                projects = user.pop('projects')
-                user['project_ids'] = {x["id"] for x in projects}
-                res.append(UserInfo.parse_obj(user))
-            return [x.get_dict() for x in res]
-
-
-async def get_notebook_list(token, filter_path=None):
-    res = []
-    async with aiohttp.ClientSession() as session:
-        # url = f"{ENV_COMMON_URL}{NOTEBOOK_PREFIX_URL}?filter[project__code]={project_code}"
-        url = f"http://{NOTEBOOK_SERVICE_URL}{NOTEBOOK_PREFIX_URL}/items"
-        headers = {
-            'Authorization': token,
-            'Content-Type': 'application/json'
-        }
-        if filter_path:
-            url += filter_path
-        # print(url)
-        async with session.get(url, headers=headers) as response:
-            # print("status:{}".format(response.status))
-            text = await response.json()
-            note_data = text['result']
-            for note in note_data:
-                note['project_id'] = note['project']['id']
-                res.append(NotebookInfo.parse_obj(note))
-    return [x.get_dict() for x in res]
 
 
 async def get_notebook_job_list_by_server(server_ip):
@@ -180,27 +60,4 @@ async def get_notebook_job_list_by_server(server_ip):
         print(note)
         print("job``````````")
         res.append(PodInfoByServer.parse_obj(note))
-    return [x.get_dict() for x in res]
-
-
-async def get_job_list(token, filter_path=None):
-    res = []
-    async with aiohttp.ClientSession() as session:
-        # url = f"{ENV_COMMON_URL}{NOTEBOOK_PREFIX_URL}?filter[project__code]={project_code}"
-        url = f"http://{JOB_SERVICE_URL}{JOB_PREFIX_URL}/items"
-        headers = {
-            'Authorization': token,
-            'Content-Type': 'application/json'
-        }
-        if filter_path:
-            url += filter_path
-        # print(url)
-        async with session.get(url, headers=headers) as response:
-            # print("status:{}".format(response.status))
-            text = await response.json()
-            job_data = text['result']
-            # print(job_data)
-            for job in job_data:
-                job['project_id'] = job['project']['id']
-                res.append(JobInfo.parse_obj(job))
     return [x.get_dict() for x in res]
