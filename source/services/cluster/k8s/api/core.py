f2644d5e3242fa1ccc7a43006254045df9b5f843
jiangshouchen <shouchen.jiang@digitalbrain.cn>
2023-03-14 18:40:34 +0800

    fix bug

--- source/services/cluster/k8s/api/core.py
+++ source/services/cluster/k8s/api/core.py
@@ -1,6 +1,6 @@
 import os
 from kubernetes.config import load_kube_config, load_incluster_config
-from kubernetes.client import CoreV1Api, CustomObjectsApi, StorageV1Api
+from kubernetes.client import CoreV1Api, CustomObjectsApi, StorageV1Api, AppsV1Api
 
 
 class Core:
@@ -9,6 +9,7 @@ class Core:
         self._core_v1_api = CoreV1Api()
         self._custom_object_api = CustomObjectsApi()
         self._storage_v1_api = StorageV1Api()
+        self._apps_v1_api = AppsV1Api()
 
     def _find_kubeconfig(self):
         if os.getenv("KUBECONFIG"):
@@ -27,3 +28,7 @@ class Core:
     @property
     def storage_v1_api(self):
         return self._storage_v1_api
+
+    @property
+    def apps_v1_api(self):
+        return self._apps_v1_api
