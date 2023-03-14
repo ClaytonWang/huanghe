71077cf3cb92f8250d95e8cccefc40a4fdda5cfc
jiangshouchen <shouchen.jiang@digitalbrain.cn>
2023-03-14 20:04:59 +0800

    fix bug

--- source/services/cluster/deployment/api.py
+++ source/services/cluster/deployment/api.py
@@ -3,30 +3,30 @@ from services.cluster.deployment.serializers import Deployment, DeploymentDelete
 from services.cluster.k8s.cluster_client import cc
 from basic.middleware.rsp import success_common_response
 
-router_notebook = APIRouter()
+router_deployment = APIRouter()
 
 
-@router_notebook.post(
+@router_deployment.post(
     '',
     description='创建notebook',
 )
 def create_notebook(dcr: DeploymentCreateReq):
-    cc.create_notebook(Deployment.parse_obj(dcr.gen_notebook_dict()))
+    cc.create_deployment(Deployment.parse_obj(dcr.gen_notebook_dict()))
     return success_common_response()
 
 
-@router_notebook.post(
+@router_deployment.post(
     '/batch',
     description='批量查询notebook',
 )
-def list_notebook(nblr: NoteBookListReq):
-    return cc.list_notebook(nblr)
+def list_notebook(dlr: DeploymentListReq):
+    return cc.list_deployment(dlr)
 
 
-@router_notebook.delete(
+@router_deployment.delete(
     '',
     description='删除notebook',
 )
-def delete_notebook(nb: NoteBookDeleteReq):
-    cc.delete_notebook(nb)
+def delete_notebook(ddr: DeploymentDeleteReq):
+    cc.delete_deployment(ddr)
     return success_common_response()
