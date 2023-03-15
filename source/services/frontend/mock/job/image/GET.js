d42f9a76bc6e5ef689eadb9f22de828b84a8c750
guanlin.li <guanlin.li@digitalbrain.cn>
2023-03-15 21:20:11 +0800

    feat: AIDP-367

---
+++ source/services/frontend/mock/job/image/GET.js
@@ -0,0 +1,26 @@
+/*
+ * @Author: guanlin.li guanlin.li@digitalbrain.cn
+ * @Date: 2023-03-14 10:04:30
+ * @LastEditors: guanlin.li guanlin.li@digitalbrain.cn
+ * @LastEditTime: 2023-03-14 10:13:57
+ * @FilePath: /huanghe/source/services/frontend/mock/job/image/GET.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+module.exports = {
+  success: true,
+  message: '',
+  result: {
+    data: [
+      {
+        desc: 'xxx', // 镜像描述
+        name: 'tensorflow-01',
+        id: '1',
+      },
+      {
+        desc: 'xxx', // 镜像描述
+        name: 'tensorflow-02',
+        id: '2',
+      },
+    ],
+  },
+};
