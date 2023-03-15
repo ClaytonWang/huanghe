00940616fb0ef45b1230924015212f84ab04c7ea
menglingbo <lingbo.meng@digitalbrain.cn>
2023-03-15 17:45:01 +0800

    delete print and add requirements for test

--- source/services/monitor/utils/service_requests.py
+++ source/services/monitor/utils/service_requests.py
@@ -36,28 +36,18 @@ class PodInfoByServer(BaseModel):
 
 async def get_notebook_job_list_by_server(server_ip):
     res = []
-    # response = requests.get(f"http://127.0.0.1:8012/notebooks/by_server/{server_ip}",
-    #                         headers={'Content-Type': 'application/json'})
     response = requests.get(f"http://{NOTEBOOK_SERVICE_URL}{NOTEBOOK_PREFIX_URL}/by_server/{server_ip}",
                             headers={'Content-Type': 'application/json'})
     text_notebook = response.json()
-    print(text_notebook)
     notebook_data = text_notebook['result']
     for note in notebook_data:
-        print("``````````")
-        print(note)
-        print("``````````")
+
         res.append(PodInfoByServer.parse_obj(note))
 
-    # response_job = requests.get(f"http://127.0.0.1:8013/jobs/by_server/{server_ip}",
-    #                             headers={'Content-Type': 'application/json'})
     response_job = requests.get(f"http://{JOB_SERVICE_URL}{JOB_PREFIX_URL}/by_server/{server_ip}",
                                 headers={'Content-Type': 'application/json'})
     text_job = response_job.json()
     job_data = text_job['result']
     for note in job_data:
-        print("job``````````")
-        print(note)
-        print("job``````````")
         res.append(PodInfoByServer.parse_obj(note))
     return [x.get_dict() for x in res]
