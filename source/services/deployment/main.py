c9913c60e4651888cea48bde6f111a5da1c4ac05
jiangshouchen <shouchen.jiang@digitalbrain.cn>
2023-04-06 14:28:58 +0800

    fix bug

--- source/services/deployment/main.py
+++ source/services/deployment/main.py
@@ -4,7 +4,7 @@
 
 import uvicorn
 from config import *
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from starlette.middleware.base import BaseHTTPMiddleware
 from asyncpg.exceptions import PostgresError
 from pydantic.error_wrappers import ValidationError
@@ -22,7 +22,7 @@ from ormar.exceptions import AsyncOrmException
 from deployment.api import router_deployment
 from basic.middleware.account_getter import verify_token
 from basic.common.initdb import startup_event, shutdown_event
-
+from basic.middleware.service_requests import get_source_list
 
 # oauth2_scheme = OFOAuth2PasswordBearer(token_url="/v1/auth/login")
 # oauth2_scheme = OFOAuth2PasswordBearer(token_url=USER_SERVICE_PATH + "/v1/auth/login")
@@ -54,6 +54,12 @@ app.add_exception_handler(AsyncOrmException, ormar_db_exception_handler)
 def status():
     return {"status": "ok"}
 
+
+@app.get('/source')
+async def get_source(request: Request):
+    authorization: str = request.headers.get('authorization')
+    res_source = await get_source_list(authorization)
+    return res_source
 
 # 路由配置
 app.include_router(router_deployment, prefix='/deployments', tags=['Deployment'])
