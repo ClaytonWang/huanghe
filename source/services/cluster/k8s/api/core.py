f188bc853492d448b7c553719ed73a307a102854
menglingbo <lingbo.meng@digitalbrain.cn>
2023-04-13 14:22:17 +0800

    add watchdog get new token and change config

--- source/services/cluster/k8s/api/core.py
+++ source/services/cluster/k8s/api/core.py
@@ -1,17 +1,32 @@
 import os
+import random
+import threading
+
+import time
 from kubernetes.config import load_kube_config, load_incluster_config
 from kubernetes.client import CoreV1Api, CustomObjectsApi, StorageV1Api, AppsV1Api
 from services.cluster.k8s.api.hw_v1beta1_api import HWV1Beta1Api
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
 
-
-class Core:
-    def __init__(self, path=None):
+class Core(FileSystemEventHandler):
+    test="origin"
+    def __init__(self, path=None,is_oberv=False):
         self._find_kubeconfig(path)
         self._core_v1_api = CoreV1Api()
         self._custom_object_api = CustomObjectsApi()
         self._storage_v1_api = StorageV1Api()
         self._apps_v1_api = AppsV1Api()
         self._v1_beta1_api = HWV1Beta1Api()
+        self.observer = Observer()
+        self.observer.unschedule_all()
+        event_handler = self
+        if is_oberv:
+            self.observer.schedule(event_handler,
+                                   path="./kubeconfig",
+                                   recursive=False)
+            self.thread = threading.Thread(target=self.observer.start)
+            self.thread.start()
 
     def _find_kubeconfig(self, path=None):
         if path:
@@ -20,6 +35,14 @@ class Core:
             load_kube_config()
         else:
             load_incluster_config()
+    def on_any_event(self, event):
+        print("!!!!!!!!!!!modified!!!!!!!!!!!!    ")
+        self.change()
+        print("!!!!!!!!!!!modified!!!!!!!!!!!!    ")
+    def change(self):
+        print("!!!!!!!!!!!change!!!!!!!!!!!!    ")
+        self.test = random.randint(0, 9999)
+        print(self.test)
 
     @property
     def core_v1_api(self):
@@ -50,6 +73,7 @@ class K8sConfigFactory:
 
     def __init__(self):
         if not self.pool:
+            print("not pool")
             for file in os.listdir(kubeconfig_path):
                 self.pool[file] = Core(os.path.join(kubeconfig_path, file))
         self.pool["default"] = Core()
@@ -66,3 +90,26 @@ class K8sConfigFactory:
         return self.pool[key]
 
 kcf = K8sConfigFactory()
+print("create core end")
+
+print("Event loop starting...")
+class ExampleThread(threading.Thread):
+    def __init__(self, obj, property):
+        threading.Thread.__init__(self)
+        self.obj = obj
+        self.property = property
+
+
+    def run(self):
+        print("thread run")
+        print(getattr(self.obj, self.property))
+        print("done")
+        # A.loop.run_forever()
+        while True:
+            print(getattr(self.obj, self.property))
+
+            time.sleep(5)
+
+
+print("thread start")
+ExampleThread(kcf.pool["default"], "test").start()
